d9c9b618ec129b92c65edbe8df5d9eb660236263
George Sergeev <gsergeev@context-it.ru>
2018-10-25 15:46:22 +0300

    Add rollback to child method

--- src/main/java/com/example/postgresdemo/service/QuestionService.java
+++ src/main/java/com/example/postgresdemo/service/QuestionService.java
@@ -5,6 +5,7 @@ import com.example.postgresdemo.model.Answer;
 import com.example.postgresdemo.model.Question;
 import com.example.postgresdemo.repository.AnswerRepository;
 import com.example.postgresdemo.repository.QuestionRepository;
+import netscape.security.ForbiddenTargetException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,26 +26,31 @@ public class QuestionService {
     @Transactional(rollbackFor = ResourceNotFoundException.class)
     public Question createQuestionAndAnswers (Question question) {
         Question newQuestion = questionRepository.save(question);
-        List <Answer> answList = new ArrayList<>();
         for (int i=0; i<10; i++) {
             Answer answ = new Answer();
             answ.setText(String.format("Answer %d to question %d",i,question.getId().intValue()));
             try {
-                //createAnswer(newQuestion.getId(), answ); //correct
-                createAnswer(newQuestion.getId()+1, answ); //throw exception
+                createAnswer(newQuestion.getId(), answ); //correct
+      //          createAnswer(newQuestion.getId()+1, answ); //throw exception
             } catch (ResourceNotFoundException ex) {
                 System.out.println("Write log");
                 throw ex;
+            } catch (ForbiddenTargetException exF) {
+                System.out.println("Write log: Forbidden to creating");
             }
-            answList.add(answ);
         }
         return  newQuestion;
     }
 
+    @Transactional(rollbackFor = ForbiddenTargetException.class)
     public  Answer createAnswer (Long questionId, Answer answer) {
         return questionRepository.findById(questionId)
                 .map(question -> {
                     answer.setQuestion(question);
+                    if (answer.getText().contains("5")) {
+                        System.out.println("Exception for "+answer.getText());
+                        throw new ForbiddenTargetException();
+                    }
                     return answerRepository.save(answer);
                 }).orElseThrow(() -> new ResourceNotFoundException("Question not found with id " + questionId));
     }
